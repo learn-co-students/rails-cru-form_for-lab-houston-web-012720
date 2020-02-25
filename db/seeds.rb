91b6bc8e87f39824b26badbd3fcc8bf6fd5d0047
DrVonDevious <drvondevious@gmail.com>
2020-02-24 22:04:09 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+Artist.create(name: "Snoop Dog", bio: "If the ride is more fly, then you must buy.")
+
+Genre.create(name: "Hip-Hop")
+
+Song.create(name: "Drop It Like It's Hot", genre_id: 1, artist_id: 1)
