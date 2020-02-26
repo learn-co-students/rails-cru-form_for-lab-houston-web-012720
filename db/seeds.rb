67e230f01de7b9571079cd6e7c9c5d3e810c5171
anamsoomro <anamsoomroed@gmail.com>
2020-02-25 18:14:55 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+a1 = Artist.create(name: "DaBaby", bio: "my name is dababy")
+a2 = Artist.create(name: "Tiny Tim", bio: "i am tiny time")
+
+g1 = Genre.create(name: "Rap")
+g2 = Genre.create(name: "Creepy")
+
+s1 = Song.create(name: "Suge", artist_id: a1.id, genre_id: g1.id)
+s2 = Song.create(name: "Tiptoe Throught the Tulips", artist_id: a2.id, genre_id: g2.id)
+
